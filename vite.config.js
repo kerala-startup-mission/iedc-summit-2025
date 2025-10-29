402a5ea119d610be14a1e7a5502f995f358df47a
Umar Al Mukhtar <umar1868807@gmail.com>
2025-10-29 21:15:23 +0530

    fix build error

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    minify: "terser",
-    cssMinify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          router: ["react-router-dom"],
-        },
-      },
-    },
-  },
-  server: {
-    headers: {
-      "Cache-Control": "public, max-age=31536000",
-      "X-Content-Type-Options": "nosniff",
-      "X-Frame-Options": "DENY",
-      "X-XSS-Protection": "1; mode=block",
-    },
-  },
 });
